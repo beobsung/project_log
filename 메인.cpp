d558314c8286c77375b60d6801826b3deb25067a
beobsung <44564964+beobsung@users.noreply.github.com>
2020-10-20 14:42:40 +0900

    Add files via upload

---
+++ 메인.cpp
@@ -0,0 +1,14 @@
+#include "Project6.h"
+
+
+int main() {
+	LOG(INFO, "여기 인포");
+	//while (1) {
+	//	getchar();
+	//	LOG(INFO, "여기 인포");
+	//	getchar();
+	//	LOG(DEBUG, "여기 디버그");
+	//	getchar();
+	//	LOG(ERROR, "여기 에러");
+	//}
+}
